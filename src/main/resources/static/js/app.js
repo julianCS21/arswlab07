5eb931fb5e9d91896bbb4a5a4b4bb18b14eb71ab
CamiloCanta <camiloct35@gmail.com>
2023-03-09 10:04:21 -0500

    avance

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,10 @@
+function Blueprint(author,blueprints){
+    let _author = author;
+    let _blueprints = blueprints;
+
+
+    this.setAuthor = function(newAuthor){
+        _author = newAuthor;
+
+    }
+}
